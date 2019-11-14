de603aa10aada818c5ee5c626edbd976d2df25de
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2019-11-14 15:04:10 +0700

    Add Full Sach

--- QuanLyCuaHangSach/Areas/Admin/Controllers/SachController.cs
+++ QuanLyCuaHangSach/Areas/Admin/Controllers/SachController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuanLyCuaHangSach.Data;
+using QuanLyCuaHangSach.Models;
 using QuanLyCuaHangSach.Models.ViewModel;
 using QuanLyCuaHangSach.Utility;
 
@@ -81,6 +82,141 @@ namespace QuanLyCuaHangSach.Areas.Admin.Controllers
             }
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+
+        //GET : Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            SachVM.Sach = await _db.Sach.Include(m => m.NhaXuatBan).Include(m => m.TacGia).Include(m => m.TheLoai).SingleOrDefaultAsync(m => m.IDSach == id);
+
+            if (SachVM.Sach == null)
+            {
+                return NotFound();
+            }
+
+            return View(SachVM);
+        }
+
+
+        //Post : Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+
+                var sachFromDb = _db.Sach.Where(m => m.IDSach == SachVM.Sach.IDSach).FirstOrDefault();
+
+                if (files.Count > 0 && files[0] != null)
+                {
+                    //if user uploads a new image
+                    var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                    var extension_new = Path.GetExtension(files[0].FileName);
+                    var extension_old = Path.GetExtension(sachFromDb.Anh);
+
+                    if (System.IO.File.Exists(Path.Combine(uploads, SachVM.Sach.IDSach + extension_old)))
+                    {
+                        System.IO.File.Delete(Path.Combine(uploads, SachVM.Sach.IDSach + extension_old));
+                    }
+                    using (var filestream = new FileStream(Path.Combine(uploads, SachVM.Sach.IDSach + extension_new), FileMode.Create)) 
+                    {
+                        files[0].CopyTo(filestream);
+                    }
+                    SachVM.Sach.Anh = @"\" + SD.ImageFolder + @"\" + SachVM.Sach.IDSach + extension_new;
+                }
+
+                if (SachVM.Sach.Anh != null)
+                {
+                    sachFromDb.Anh = SachVM.Sach.Anh;
+                }
+
+                sachFromDb.TenSach = SachVM.Sach.TenSach;
+                sachFromDb.Gia = SachVM.Sach.Gia;
+                sachFromDb.NamXB = SachVM.Sach.NamXB;
+                sachFromDb.CoSan = SachVM.Sach.CoSan;
+                sachFromDb.IDTheLoai = SachVM.Sach.IDTheLoai;
+                sachFromDb.IDTacGia = SachVM.Sach.IDTacGia;
+                sachFromDb.IDNhaXuatBan = SachVM.Sach.IDNhaXuatBan;
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(SachVM);
+        }
+
+
+        //GET : Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            SachVM.Sach = await _db.Sach.Include(m => m.NhaXuatBan).Include(m => m.TacGia).Include(m => m.TheLoai).SingleOrDefaultAsync(m => m.IDSach == id);
+
+            if (SachVM.Sach == null)
+            {
+                return NotFound();
+            }
+
+            return View(SachVM);
+        }
+
+        //GET : Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            SachVM.Sach = await _db.Sach.Include(m => m.NhaXuatBan).Include(m => m.TacGia).Include(m => m.TheLoai).SingleOrDefaultAsync(m => m.IDSach == id);
+
+            if (SachVM.Sach == null)
+            {
+                return NotFound();
+            }
+
+            return View(SachVM);
+        }
+
+        //POST : Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            Sach sach = await _db.Sach.FindAsync(id);
+
+            if (sach == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                var extension = Path.GetExtension(sach.Anh);
+
+                if (System.IO.File.Exists(Path.Combine(uploads, sach.IDSach + extension)))
+                {
+                    System.IO.File.Delete(Path.Combine(uploads, sach.IDSach + extension));
+                }
+                _db.Sach.Remove(sach);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
         }
     }
 }
