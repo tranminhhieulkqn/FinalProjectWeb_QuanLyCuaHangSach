f0344f2f775ee66f6d771ee8b4f5ab7c68587b04
Trần Minh Hiếu <46083795+tranminhhieulkqn@users.noreply.github.com>
2019-11-19 17:35:31 +0700

    Done Giao Dich

--- QuanLyCuaHangSach/Areas/Admin/Controllers/GiaoDichController.cs
+++ QuanLyCuaHangSach/Areas/Admin/Controllers/GiaoDichController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using QuanLyCuaHangSach.Data;
+using QuanLyCuaHangSach.Models;
 using QuanLyCuaHangSach.Models.ViewModel;
 using QuanLyCuaHangSach.Utility;
 
@@ -67,6 +68,131 @@ namespace QuanLyCuaHangSach.Areas.Customer.Controllers
             
 
             return View(giaoDichVM);
+        }
+
+
+        //GET Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sachList = (IEnumerable<Sach>)(from p in _db.Sach
+                                                      join a in _db.ChiTietGiaoDich
+                                                      on p.IDSach equals a.IDSach
+                                                      where a.IDGiaoDich == id
+                                                      select p).Include("TheLoai")
+                                                               .Include("TacGia")
+                                                               .Include("NhaXuatBan");
+
+            ChiTietGiaoDichViewModel chiTietGiaoDichVM = new ChiTietGiaoDichViewModel()
+            {
+                GiaoDich = _db.GiaoDich.Include(a => a.NguoiBan).Where(a => a.IDGiaoDich == id).FirstOrDefault(),
+                NguoiBan = _db.ApplicationUser.ToList(),
+                Sach = sachList.ToList()
+            };
+
+            return View(chiTietGiaoDichVM);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, ChiTietGiaoDichViewModel chiTietGiaoDichVM)
+        {
+            if (ModelState.IsValid)
+            {
+                chiTietGiaoDichVM.GiaoDich.NgayGiaoDich = chiTietGiaoDichVM.GiaoDich.NgayGiaoDich
+                                    .AddHours(chiTietGiaoDichVM.GiaoDich.GioGiaoDich.Hour)
+                                    .AddMinutes(chiTietGiaoDichVM.GiaoDich.GioGiaoDich.Minute);
+
+                var giaoDich_DB = _db.GiaoDich.Where(a => a.IDGiaoDich == chiTietGiaoDichVM.GiaoDich.IDGiaoDich).FirstOrDefault();
+
+                giaoDich_DB.TenKhachHang = chiTietGiaoDichVM.GiaoDich.TenKhachHang;
+                giaoDich_DB.EmailKhachHang = chiTietGiaoDichVM.GiaoDich.EmailKhachHang;
+                giaoDich_DB.SDTKhachHang = chiTietGiaoDichVM.GiaoDich.SDTKhachHang;
+                giaoDich_DB.NgayGiaoDich = chiTietGiaoDichVM.GiaoDich.NgayGiaoDich;
+                giaoDich_DB.XacNhan = chiTietGiaoDichVM.GiaoDich.XacNhan;
+                if (User.IsInRole(SD.SuperAdminEndUser))
+                {
+                    giaoDich_DB.IDNguoiBan = chiTietGiaoDichVM.GiaoDich.IDNguoiBan;
+                }
+                _db.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+
+
+            }
+
+            return View(chiTietGiaoDichVM);
+        }
+
+        //GET Detials
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sachList = (IEnumerable<Sach>)(  from p in _db.Sach
+                                                    join a in _db.ChiTietGiaoDich
+                                                    on p.IDSach equals a.IDSach
+                                                    where a.IDGiaoDich == id
+                                                    select p).Include("TheLoai")
+                                                            .Include("TacGia")
+                                                            .Include("NhaXuatBan");
+
+            ChiTietGiaoDichViewModel chiTietGiaoDichVM = new ChiTietGiaoDichViewModel()
+            {
+                GiaoDich = _db.GiaoDich.Include(a => a.NguoiBan).Where(a => a.IDGiaoDich == id).FirstOrDefault(),
+                NguoiBan = _db.ApplicationUser.ToList(),
+                Sach = sachList.ToList()
+            };
+
+            return View(chiTietGiaoDichVM);
+
+        }
+
+
+        //GET Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sachList = (IEnumerable<Sach>)(from p in _db.Sach
+                                               join a in _db.ChiTietGiaoDich
+                                               on p.IDSach equals a.IDSach
+                                               where a.IDGiaoDich == id
+                                               select p).Include("TheLoai")
+                                                            .Include("TacGia")
+                                                            .Include("NhaXuatBan");
+
+            ChiTietGiaoDichViewModel chiTietGiaoDichVM = new ChiTietGiaoDichViewModel()
+            {
+                GiaoDich = _db.GiaoDich.Include(a => a.NguoiBan).Where(a => a.IDGiaoDich == id).FirstOrDefault(),
+                NguoiBan = _db.ApplicationUser.ToList(),
+                Sach = sachList.ToList()
+            };
+
+            return View(chiTietGiaoDichVM);
+
+        }
+
+
+        //POST Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var giaoDich = await _db.GiaoDich.FindAsync(id);
+            _db.GiaoDich.Remove(giaoDich);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
